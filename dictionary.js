32a581ce74a4b3431b12caac77e5427adb8f9214
Marco <mondial95@gmail.com>
2018-10-04 03:19:49 +0200

    ✨po pulated dictionary

--- dictionary.js
+++ dictionary.js
@@ -3,10 +3,46 @@
  */
 const ARTICLES      = ['a', 'an', 'the',]
 const PREPOSITIONS  = [
-  'after', 'in', 'to', 'on',
+  'aboard', 'about', 'above', 'across', 'after',
+  'against', 'along', 'amid', 'among', 'around',
+  'as', 'at', 'before', 'behind', 'below',
+  'beneath', 'beside', 'between', 'beyond', 'but',
+  'by', 'concerning', 'considering', 'despite', 'down',
+  'during', 'except', 'following', 'for', 'from',
+  'in', 'inside', 'into', 'like', 'minus',
+  'near', 'next', 'of', 'off', 'on',
+  'onto', 'opposite', 'out', 'outside', 'over',
+  'past', 'per', 'plus', 'regarding', 'round',
+  'save', 'since', 'than', 'through', 'to',
+  'toward', 'under', 'underneath', 'unlike', 'until',
+  'up', 'upon', 'versus', 'via', 'with',
+  'within', 'without',
 ]
 const PRONOUNS      = [
-  'i', 'me', 'mine', 'you', 'yours', 'his', 'her', 'hers', 'we', 'they', 'them',
+  'all', 'another', 'any', 'anybody', 'anyone',
+  'anything', 'as', 'aught', 'both', 'each',
+  'either', 'enough', 'everybody', 'everyone', 'everything',
+  'few', 'he', 'her', 'hers', 'herself',
+  'him', 'himself', 'his', 'i', 'idem',
+  'it', 'its', 'itself', 'many', 'me',
+  'mine', 'most', 'my', 'myself', 'naught',
+  'neither', 'nobody', 'none', 'nothing', 'nought',
+  'one', 'other', 'others', 'ought', 'our',
+  'ours', 'ourself', 'ourselves', 'several', 'she',
+  'some', 'somebody', 'someone', 'something', 'somewhat',
+  'such', 'suchlike', 'that', 'thee', 'their',
+  'theirs', 'theirself', 'theirselves', 'them', 'themself',
+  'themselves', 'there', 'these', 'they', 'thine',
+  'this', 'those', 'thou', 'thy', 'thyself',
+  'us', 'we', 'what', 'whatever', 'whatnot',
+  'whatsoever', 'whence', 'where', 'whereby', 'wherefrom',
+  'wherein', 'whereinto', 'whereof', 'whereon', 'wherever',
+  'wheresoever', 'whereto', 'whereunto', 'wherewith', 'wherewithal',
+  'whether', 'which', 'whichever', 'whichsoever', 'who',
+  'whoever', 'whom', 'whomever', 'whomso', 'whomsoever',
+  'whose', 'whosever', 'whosesoever', 'whoso', 'whosoever',
+  'ye', 'yon', 'yonder', 'you', 'your',
+  'yours', 'yourself', 'yourselves',
 ]
 
 /**
